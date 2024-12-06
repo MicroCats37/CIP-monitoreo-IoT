a42b8b0c167b15ab7d2ab13b9273e5afe006dd19
MicroCats37 <microcat_73@outlook.com>
2024-12-06 14:47:14 -0500

    se mejoro la ui de todas las paginas solo faltaira el login y side bar y back de autentifiacion

--- src/store/mqttStore.ts
+++ src/store/mqttStore.ts
@@ -10,8 +10,33 @@ const schemas = [
   ArrayWaterPumpTypeSchema,
   ArrayBoardTypeSchema,
   ArrayVariatorsTypeSchema,
-  
 ];
+
+
+const topicNames: { [key: string]: string } = {
+  "dashboard/aire-acondicionado/1": "Aire Acondicionado 1",
+  "dashboard/aire-acondicionado/2": "Aire Acondicionado 2",
+  'dashboard/bombas/agua-potable': "Agua Potable",
+  'dashboard/bombas/aguas-tratadas': "Aguas Tratadas",
+  'dashboard/bombas/aguas-grises': "Agua Grises",
+  'dashboard/estacionamientos/sotano/1a': "Estacionamiento Sotano 1A",
+  'dashboard/estacionamientos/sotano/2a': "Estacionamiento Sotano 1B",
+  'dashboard/estacionamientos/sotano/3a': "Estacionamiento Sotano 2A",
+  'dashboard/estacionamientos/sotano/4a': "Estacionamiento Sotano 2B",
+  'dashboard/estacionamientos/sotano/1b': "Estacionamiento Sotano 3A",
+ 'dashboard/estacionamientos/sotano/2b': "Estacionamiento Sotano 3B",
+  'dashboard/estacionamientos/sotano/3b': "Estacionamiento Sotano 4A",
+  'dashboard/estacionamientos/sotano/4b': "Estacionamiento Sotano 4B",
+  'dashboard/sci': "Sistema Contra Incendios",
+  'dashboard/tableros': "Tableros Electricos",
+  'dashboard/variadores/agua-potable': "Variadores Agua Potable",
+  'dashboard/variadores/aguas-tratadas': "Variadores Aguas Tratadase",
+  'dashboard/variadores/aguas-grises': "Variadores Aguas Grises",
+}
+
+const getTopicName = (topic:string) => {
+  return topicNames[topic] || `Dispositivo desconocido para el topic: ${topic}`;
+};
 
 
 // Unificar todos los tipos de mensaje posibles
@@ -53,7 +78,8 @@ export const useMqttStore = create<MqttStore>()(
             console.error(`Error al suscribirse al tópico ${topic}:`, err);
           } else {
             console.log(`Suscrito al tópico: ${topic}`);
-            toast.success(`Conexión MQTT establecida para ${topic}`);
+            const topicName = getTopicName(topic);
+            toast.success(`Conexión MQTT establecida para ${topicName}`);
           }
         });
 
